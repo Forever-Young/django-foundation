e8e5a0052020d74197cfb130025f3819aa68a487
Apanovich Stanislav <inbox@spacetekk.ru>
2013-07-03 03:54:25 +0400

    grid class changed to small

--- foundation/forms.py
+++ foundation/forms.py
@@ -4,8 +4,8 @@ from django import forms
 
 def form_as_grid_list(form, cols=10):
     cols = getattr(form, 'grid_cols', cols)
-    normal_row = '<div class="row formrow"><div class="large-' + str(cols)\
-        + ' large-centered columns">%(label)s%(field)s%(errors)s%(help_text)s'\
+    normal_row = '<div class="row formrow"><div class="small-' + str(cols)\
+        + ' small-centered columns">%(label)s%(field)s%(errors)s%(help_text)s'\
         + '</div></div>'
     output = form._html_output(
         normal_row = normal_row,
@@ -17,9 +17,9 @@ def form_as_grid_list(form, cols=10):
 
 
 def form_as_grid_pair_list(form):
-    normal_row ='<div class="row formrow"><div class="large-4 large-offset-1 columns">'\
-        + '%(label)s</div><div class="large-6 columns">%(field)s%(errors)s%(help_text)s'\
-        + '</div><div class="large-1 columns">&emsp;</div></div>'
+    normal_row ='<div class="row formrow"><div class="small-4 small-offset-1 columns">'\
+        + '%(label)s</div><div class="small-6 columns">%(field)s%(errors)s%(help_text)s'\
+        + '</div><div class="small-1 columns">&emsp;</div></div>'
     output = form._html_output(
         normal_row = normal_row,
         error_row = u'<div class="hide">%s</div>',
