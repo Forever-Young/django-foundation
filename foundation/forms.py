f411936c3122521eb402869aac9b2b9435ce7605
space::tekk <inbox@spacetekk.ru>
2013-10-17 23:15:48 +0400

    fixes

--- foundation/forms.py
+++ foundation/forms.py
@@ -17,9 +17,9 @@ def form_as_grid_list(form, cols=10):
 
 
 def form_as_grid_pair_list(form):
-    normal_row ='<div class="row formrow"><div class="small-4 small-offset-1 columns">'\
-        + '%(label)s</div><div class="small-6 columns">%(field)s%(errors)s%(help_text)s'\
-        + '</div><div class="small-1 columns">&emsp;</div></div>'
+    normal_row ='<div class="row formrow"><div class="small-5 columns">'\
+        + '%(label)s</div><div class="small-7 columns">%(field)s%(errors)s%(help_text)s'\
+        + '</div></div>'
     output = form._html_output(
         normal_row = normal_row,
         error_row = u'<div class="hide">%s</div>',
