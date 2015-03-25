0b254ecdf573b967e3c0535928a824b773e33c0f
Anton Novosyolov <anton.novosyolov@gmail.com>
2015-03-25 21:58:14 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-foundation',
+    version='1.0.0',
+    description='Django integration with Zurb Foundation framework',
+    author='https://github.com/spacetekk',
+    url='https://github.com/spacetekk/django-foundation',
+    packages=find_packages(),
+)
